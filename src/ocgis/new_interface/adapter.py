117c4f0dc44ffce3ae920478b9cc331ee55779a9
bekozi <benkoziol@gmail.com>
2015-08-15 11:46:25 -0600

    mark - before moving boundedvariable to inherit from variable

--- src/ocgis/new_interface/adapter.py
+++ src/ocgis/new_interface/adapter.py
@@ -1,8 +1,10 @@
 from abc import abstractmethod
 
+import numpy as np
+
 from ocgis.new_interface.variable import Variable
 from ocgis.util.helpers import get_bounds_from_1d, get_formatted_slice
-from ocgis.exc import BoundsAlreadyAvailableError
+from ocgis.exc import BoundsAlreadyAvailableError, EmptySubsetError
 from ocgis.new_interface.base import AbstractInterfaceObject
 
 
@@ -36,6 +38,70 @@ class BoundedVariable(AbstractAdapter):
         else:
             bounds = None
         return BoundedVariable(variable, bounds=bounds)
+
+    def get_between(self, lower, upper, return_indices=False, closed=False, use_bounds=True):
+        assert (lower <= upper)
+
+        # Determine if data bounds are contiguous (if bounds exists for the data). Bounds must also have more than one
+        # row.
+        is_contiguous = False
+        if self.bounds is not None:
+            try:
+                if len(set(self.bounds[0, :]).intersection(set(self.bounds[1, :]))) > 0:
+                    is_contiguous = True
+            except IndexError:
+                # There is likely not a second row.
+                if self.bounds.shape[0] == 1:
+                    pass
+                else:
+                    raise
+
+        # Subset operation when bounds are not present.
+        if self.bounds is None or use_bounds == False:
+            value = self.variable.value
+            if closed:
+                select = np.logical_and(value > lower, value < upper)
+            else:
+                select = np.logical_and(value >= lower, value <= upper)
+        # Subset operation in the presence of bounds.
+        else:
+            # Determine which bound column contains the minimum.
+            if self.bounds[0, 0] <= self.bounds[0, 1]:
+                lower_index = 0
+                upper_index = 1
+            else:
+                lower_index = 1
+                upper_index = 0
+            # Reference the minimum and maximum bounds.
+            bounds_min = self.bounds[:, lower_index]
+            bounds_max = self.bounds[:, upper_index]
+
+            # If closed is True, then we are working on a closed interval and are not concerned if the values at the
+            # bounds are equivalent. It does not matter if the bounds are contiguous.
+            if closed:
+                select_lower = np.logical_or(bounds_min > lower, bounds_max > lower)
+                select_upper = np.logical_or(bounds_min < upper, bounds_max < upper)
+            else:
+                # If the bounds are contiguous, then preference is given to the lower bound to avoid duplicate
+                # containers (contiguous bounds share a coordinate).
+                if is_contiguous:
+                    select_lower = np.logical_or(bounds_min >= lower, bounds_max > lower)
+                    select_upper = np.logical_or(bounds_min <= upper, bounds_max < upper)
+                else:
+                    select_lower = np.logical_or(bounds_min >= lower, bounds_max >= lower)
+                    select_upper = np.logical_or(bounds_min <= upper, bounds_max <= upper)
+            select = np.logical_and(select_lower, select_upper)
+
+        if select.any() == False:
+            raise EmptySubsetError(origin=self.variable.name)
+
+        ret = self[select]
+
+        if return_indices:
+            indices = np.arange(select.shape[0])
+            ret = (ret, indices[select])
+
+        return ret
 
     def set_extrapolated_bounds(self, name=None):
         """Set the bounds variable using extrapolation."""
--- src/ocgis/new_interface/test_adapter.py
+++ src/ocgis/new_interface/test_adapter.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from ocgis.exc import EmptySubsetError
+
 from ocgis.new_interface.adapter import BoundedVariable, AbstractAdapter
 from ocgis.new_interface.dimension import Dimension
 from ocgis.new_interface.test_new_interface import AbstractTestNewInterface
@@ -31,6 +33,77 @@ class TestBoundedVariable(AbstractTestNewInterface):
         bv = self.get()
         sub = bv[1]
         self.assertNumpyAll(sub.bounds.value, bv.bounds[1, :].value)
+
+    def test_get_between(self):
+        # vdim = VectorDimension(value=[0])
+        var = Variable('foo', value=[0])
+        bv = BoundedVariable(var)
+        with self.assertRaises(EmptySubsetError):
+            bv.get_between(100, 200)
+
+        # vdim = VectorDimension(value=[100, 200, 300, 400])
+        var = Variable('foo', value=[100, 200, 300, 400])
+        bv = BoundedVariable(var)
+        vdim_between = bv.get_between(100, 200)
+        self.assertEqual(len(vdim_between), 2)
+
+    def test_get_between_bounds(self):
+        value = [0., 5., 10.]
+        bounds = [[-2.5, 2.5], [2.5, 7.5], [7.5, 12.5]]
+
+        # # a reversed copy of these bounds are created here
+        value_reverse = deepcopy(value)
+        value_reverse.reverse()
+        bounds_reverse = deepcopy(bounds)
+        bounds_reverse.reverse()
+        for ii in range(len(bounds)):
+            bounds_reverse[ii].reverse()
+
+        data = {'original': {'value': value, 'bounds': bounds},
+                'reversed': {'value': value_reverse, 'bounds': bounds_reverse}}
+        for key in ['original', 'reversed']:
+            vdim = VectorDimension(value=data[key]['value'],
+                                   bounds=data[key]['bounds'])
+
+            vdim_between = vdim.get_between(1, 3)
+            self.assertEqual(len(vdim_between), 2)
+            if key == 'original':
+                self.assertEqual(vdim_between.bounds.tostring(),
+                                 '\x00\x00\x00\x00\x00\x00\x04\xc0\x00\x00\x00\x00\x00\x00\x04@\x00\x00\x00\x00\x00\x00\x04@\x00\x00\x00\x00\x00\x00\x1e@')
+            else:
+                self.assertEqual(vdim_between.bounds.tostring(),
+                                 '\x00\x00\x00\x00\x00\x00\x1e@\x00\x00\x00\x00\x00\x00\x04@\x00\x00\x00\x00\x00\x00\x04@\x00\x00\x00\x00\x00\x00\x04\xc0')
+            self.assertEqual(vdim.resolution, 5.0)
+
+            ## preference is given to the lower bound in the case of "ties" where
+            ## the value could be assumed part of the lower or upper cell
+            vdim_between = vdim.get_between(2.5, 2.5)
+            self.assertEqual(len(vdim_between), 1)
+            if key == 'original':
+                self.assertNumpyAll(vdim_between.bounds, np.array([[2.5, 7.5]]))
+            else:
+                self.assertNumpyAll(vdim_between.bounds, np.array([[7.5, 2.5]]))
+
+            ## if the interval is closed and the subset range falls only on bounds
+            ## value then the subset will be empty
+            with self.assertRaises(EmptySubsetError):
+                vdim.get_between(2.5, 2.5, closed=True)
+
+            vdim_between = vdim.get_between(2.5, 7.5)
+            if key == 'original':
+                self.assertEqual(vdim_between.bounds.tostring(),
+                                 '\x00\x00\x00\x00\x00\x00\x04@\x00\x00\x00\x00\x00\x00\x1e@\x00\x00\x00\x00\x00\x00\x1e@\x00\x00\x00\x00\x00\x00)@')
+            else:
+                self.assertEqual(vdim_between.bounds.tostring(),
+                                 '\x00\x00\x00\x00\x00\x00)@\x00\x00\x00\x00\x00\x00\x1e@\x00\x00\x00\x00\x00\x00\x1e@\x00\x00\x00\x00\x00\x00\x04@')
+
+    def test_get_between_use_bounds(self):
+        value = [3., 5.]
+        bounds = [[2., 4.], [4., 6.]]
+        vdim = VectorDimension(value=value, bounds=bounds)
+        ret = vdim.get_between(3, 4.5, use_bounds=False)
+        self.assertNumpyAll(ret.value, np.array([3.]))
+        self.assertNumpyAll(ret.bounds, np.array([[2., 4.]]))
 
     def test_set_extrapolated_bounds(self):
         bv = self.get(with_bounds=False)
