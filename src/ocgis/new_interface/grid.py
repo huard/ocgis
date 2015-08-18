89f7b2dc7bae370ced0c16793f5242874c55c68d
bekozi <benkoziol@gmail.com>
2015-08-18 10:13:02 -0600

    mark - all tests working

--- src/ocgis/new_interface/grid.py
+++ src/ocgis/new_interface/grid.py
@@ -15,6 +15,12 @@ class Grid(AbstractInterfaceObject):
         if self.x is None or self.y is None:
             msg = 'At least "x" and "y" are required to make a grid.'
             raise ValueError(msg)
+        if self.x.ndim > 2 or self.y.ndim > 2:
+            msg = '"x" and "y" may not have ndim > 2.'
+            raise ValueError(msg)
+        if self.z is not None and self.z.ndim > 3:
+            msg = '"z" may not have ndim > 3.'
+            raise ValueError(msg)
 
     def __getitem__(self, slc):
         """
@@ -31,10 +37,22 @@ class Grid(AbstractInterfaceObject):
 
         slc = get_formatted_slice(slc, self.ndim)
         ret = copy(self)
-        ret.x = self.x[slc[1]]
-        ret.y = self.y[slc[0]]
+        if self.is_vectorized:
+            ret.x = self.x[slc]
+            ret.y = self.y[slc]
+        else:
+            ret.x = self.x[slc[0]]
+            ret.y = self.y[slc[1]]
         if self.z is not None:
             ret.z = self.z[slc[2]]
+        return ret
+
+    @property
+    def is_vectorized(self):
+        if len(self.x.shape) > 1:
+            ret = True
+        else:
+            ret = False
         return ret
 
     @property
@@ -52,8 +70,18 @@ class Grid(AbstractInterfaceObject):
 
     @property
     def shape(self):
-        ret = [len(self.x), len(self.y)]
-        if self.z is not None:
-            ret.append(len(self.z))
-        ret = tuple(ret)
+        if not self.is_vectorized:
+            ret = [len(self.x), len(self.y)]
+            if self.z is not None:
+                ret.append(len(self.z))
+            ret = tuple(ret)
+        else:
+            ret = self.x.shape
         return ret
+
+    def write_netcdf(self, dataset, **kwargs):
+        to_write = [self.x, self.y]
+        if self.z is not None:
+            to_write.append(self.z)
+        for tw in to_write:
+            tw.write_netcdf(dataset, **kwargs)
--- src/ocgis/new_interface/test_grid.py
+++ src/ocgis/new_interface/test_grid.py
@@ -1,22 +1,30 @@
-from ocgis.new_interface.base import AbstractInterfaceObject
+import numpy as np
 
+from ocgis.new_interface.base import AbstractInterfaceObject
 from ocgis.new_interface.grid import Grid
 from ocgis.new_interface.test_new_interface import AbstractTestNewInterface
 from ocgis.new_interface.variable import Variable
 
 
 class TestGrid(AbstractTestNewInterface):
-    def get(self, with_z=True):
+    def get(self, with_z=True, with_2d_variables=False):
         x = [101, 102, 103]
         y = [40, 41, 42, 43]
         z = [100, 200]
 
         kwds = {}
 
-        vx = Variable('x', value=x, dtype=float)
-        vy = Variable('y', value=y, dtype=float)
+        if with_2d_variables:
+            mx, my = np.meshgrid(x, y)
+            vx = Variable('x', value=mx, dtype=float)
+            vy = Variable('y', value=my, dtype=float)
+        else:
+            vx = Variable('x', value=x, dtype=float)
+            vy = Variable('y', value=y, dtype=float)
         kwds.update(dict(x=vx, y=vy))
+
         if with_z:
+            assert not with_2d_variables
             vz = Variable('z', value=z, dtype=float)
             kwds.update(dict(z=vz))
 
@@ -35,11 +43,20 @@ class TestGrid(AbstractTestNewInterface):
             Grid(x=x)
 
     def test_getitem(self):
+        # tdk: test with 2/3-d level
         grid = self.get()
-        sub = grid[2, 1, 0]
+        sub = grid[1, 2, 0]
         self.assertEqual(sub.x.value, 102.)
         self.assertEqual(sub.y.value, 42.)
         self.assertEqual(sub.z.value, 100.)
+
+        # Test with two-dimensional x and y values.
+        grid = self.get(with_2d_variables=True, with_z=False)
+        sub = grid[1:3, 1:]
+        actual_x = [[102.0, 103.0], [102.0, 103.0]]
+        self.assertEqual(sub.x.value.tolist(), actual_x)
+        actual_y = [[41.0, 41.0], [42.0, 42.0]]
+        self.assertEqual(sub.y.value.tolist(), actual_y)
 
     def test_resolution(self):
         grid = self.get()
@@ -52,3 +69,16 @@ class TestGrid(AbstractTestNewInterface):
 
         grid = self.get(with_z=False)
         self.assertEqual(grid.ndim, 2)
+
+        # Test with two-dimensional x and y values.
+        grid = self.get(with_2d_variables=True, with_z=False)
+        self.assertEqual(grid.shape, (4, 3))
+
+    def test_write_netcdf(self):
+        grid = self.get(with_z=True)
+        path = self.get_temporary_file_path('out.nc')
+        with self.nc_scope(path, 'w') as ds:
+            grid.write_netcdf(ds)
+        with self.nc_scope(path) as ds:
+            var = ds.variables['y']
+            self.assertNumpyAll(var[:], grid.y.value.data)
