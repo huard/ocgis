ac6dc11d00cd2d228f65e038d07c14d45f2fde9a
bekozi <benkoziol@gmail.com>
2015-12-10 13:21:43 -0800

    wip: minor

--- src/ocgis/new_interface/base.py
+++ src/ocgis/new_interface/base.py
@@ -1,16 +1,5 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 
 
 class AbstractInterfaceObject(object):
     __metaclass__ = ABCMeta
-
-
-class AbstractSpatialReferencedObject(AbstractInterfaceObject):
-    __metaclass__ = ABCMeta
-
-    def __init__(self, crs=None):
-        self.crs = crs
-
-    @abstractmethod
-    def update_crs(self, to_crs):
-        """"""
