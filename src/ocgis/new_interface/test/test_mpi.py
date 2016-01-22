573c1575e8860b6575b0afccbc3ce87fbb7d2984
bekozi <benkoziol@gmail.com>
2016-01-22 12:19:51 -0700

    wip: before chaning create slices

--- src/ocgis/new_interface/test/test_mpi.py
+++ src/ocgis/new_interface/test/test_mpi.py
@@ -3,40 +3,61 @@ import numpy as np
 from ocgis.new_interface.dimension import Dimension
 from ocgis.new_interface.test.test_new_interface import AbstractTestNewInterface
 from ocgis.new_interface.variable import Variable
+from ocgis.util.helpers import get_iter
 
 
 class Test(AbstractTestNewInterface):
     def test(self):
         # tdk: test no dimension i.e. len(dimension) == 0
 
-        def create_slices(length, n):
-            step = int(np.ceil(float(length) / n))
-            slices = [None] * n
-            start = 0
-            for ii in range(n):
-                stop = start + step
-                if stop > length:
-                    stop = length
-                index_element = slice(start, stop)
-                slices[ii] = index_element
-                start = stop
-            return slices
+        def create_slices(lengths, n):
+            ret = []
+            for length in get_iter(lengths, dtype=int):
+                step = int(np.ceil(float(length) / n))
+                print step
+                slices = [None] * n
+                start = 0
+                for ii in range(n):
+                    stop = start + step
+                    if stop > length:
+                        stop = length
+                    index_element = slice(start, stop)
+                    slices[ii] = index_element
+                    start = stop
+                ret.append(slices)
+            return ret
 
         def update_map_slices(map_slices, key, target):
             for idx, slc in enumerate(slices):
                 map_slices[idx][key] = target.__getitem__(slc)
             return map_slices
 
-        value = np.arange(100)
-        self.assertEqual(len(value), 100)
-        dimension = Dimension('t', 100)
-        n = 6
-        slices = create_slices(len(dimension), n)
-        self.assertEqual(len(slices), n)
-        actual = 0
-        for slc in slices:
-            actual += value[slc].sum()
-        self.assertEqual(actual, value.sum())
+        t = Dimension('t', 35)
+        l = Dimension('z', 11)
+        y = Dimension('y', 7)
+        x = Dimension('x', 5)
+        dimensions = [t, l, y, x]
+        variable = Variable(name='distribute_me', dimensions=dimensions)
+        n_elements = reduce(lambda i, j: i * j, variable.shape)
+        value = np.arange(n_elements).reshape(variable.shape)
+        variable.value = value
+        # print variable.value
+
+        n = 5
+        actual = create_slices([len(d) for d in variable.dimensions[2:]], n)
+        print actual
+
+        vscatter = []
+        for idx in range(n):
+            y_slice = actual[0][idx]
+            x_slice = actual[1][idx]
+            vscatter.append(variable.__getitem__({'y': y_slice, 'x': x_slice}))
+
+        for v in vscatter:
+            print v.shape
+        print actual
+
+        thh
 
         key_dimension = 'dimension'
         map_slices = {idx: {'slice': slc} for idx, slc in enumerate(slices)}
@@ -54,4 +75,3 @@ class Test(AbstractTestNewInterface):
             actual += v['variable'].value.tolist()
         self.assertEqual(np.mean(actual), value.mean())
         thh
-
