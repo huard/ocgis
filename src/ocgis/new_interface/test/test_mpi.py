ffda01a6d096a8a77235abfe76f871e614b9a861
bekozi <benkoziol@gmail.com>
2016-01-23 23:55:22 -0700

    wip: error with 5

--- src/ocgis/new_interface/test/test_mpi.py
+++ src/ocgis/new_interface/test/test_mpi.py
@@ -6,99 +6,64 @@ import numpy as np
 from shapely.geometry import box
 
 from ocgis.exc import EmptySubsetError
-from ocgis.new_interface.dimension import Dimension
 from ocgis.new_interface.geom import GeometryVariable
 from ocgis.new_interface.mpi import MPI_RANK, MPI_SIZE, MPI_COMM
 from ocgis.new_interface.test.test_new_interface import AbstractTestNewInterface
-from ocgis.new_interface.variable import Variable
 from ocgis.util.helpers import get_local_to_global_slices, get_optimal_slice_from_array
 
 
 def create_slices(size, shape):
     remaining = size
     if size == 1:
-        ret = (tuple([slice(None)] * len(shape)),)
+        ret = [tuple([slice(0, s) for s in shape])]
     else:
-        while remaining != 0:
-            slices_ii_shape = []
-            for idx_shape, ii_shape in enumerate(shape):
-                if remaining <= 0:
-                    app_slices_ii_shape = [slice(None)]
+        slices_ii_shape = []
+        for idx_shape, ii_shape in enumerate(shape):
+            if remaining <= 0:
+                app_slices_ii_shape = [slice(0, ii_shape)]
+            else:
+                if remaining < ii_shape:
+                    sections = remaining
                 else:
-                    if remaining < ii_shape:
-                        sections = remaining
-                    else:
-                        sections = ii_shape
-                    splits = np.array_split(np.arange(ii_shape), sections)
-                    slices = [get_optimal_slice_from_array(split) for split in splits]
-                    app_slices_ii_shape = slices
-                    remaining -= len(slices)
-                slices_ii_shape.append(app_slices_ii_shape)
+                    sections = ii_shape
+                # tdk: optimize: remove np.arange generation
+                splits = np.array_split(np.arange(ii_shape), sections)
+                slices = [get_optimal_slice_from_array(split) for split in splits]
+                app_slices_ii_shape = slices
+                remaining -= len(slices)
+            slices_ii_shape.append(app_slices_ii_shape)
         ret = [slices for slices in itertools.product(*slices_ii_shape)]
     return tuple(ret)
 
-
-    # ret = []
-    # for length, n in zip(get_iter(lengths, dtype=int), get_iter(ns, dtype=int)):
-    #     slices = [None] * n
-    #     start = 0
-    #     remaining = length
-    #     nlocal = n
-    #     for ii in range(n):
-    #         step = int(np.ceil(float(remaining) / nlocal))
-    #         stop = start + step
-    #         if stop > length:
-    #             stop = length
-    #         index_element = slice(start, stop)
-    #         slices[ii] = index_element
-    #         remaining -= (stop - start)
-    #         start = stop
-    #         nlocal -= 1
-    #     ret.append(slices)
-    # ret = [ii for ii in itertools.product(*ret)]
 
 class Test(AbstractTestNewInterface):
     def test_create_slices(self):
         size = 1
         shape = (4, 3)
         actual = create_slices(size, shape)
-        self.assertEqual(actual, (tuple([slice(None)] * 2),))
+        self.assertEqual(actual, ((slice(0, 4, None), slice(0, 3, None)),))
 
         size = 2
         actual = create_slices(size, shape)
-        desired = ((slice(0, 2, None), slice(None)), (slice(2, 4, None), slice(None)))
+        desired = ((slice(0, 2, None), slice(0, 3)), (slice(2, 4, None), slice(0, 3)))
         self.assertEqual(actual, desired)
-        print '#tdk !!!!!!!!!!!'
 
-    def test(self):
-        # tdk: test no dimension i.e. len(dimension) == 0
+        size = 12
+        shape = (4, 3)
+        actual = create_slices(size, shape)
+        desired = ((slice(0, 1, None), slice(0, 1, None)), (slice(0, 1, None), slice(1, 2, None)),
+                   (slice(0, 1, None), slice(2, 3, None)), (slice(1, 2, None), slice(0, 1, None)),
+                   (slice(1, 2, None), slice(1, 2, None)), (slice(1, 2, None), slice(2, 3, None)),
+                   (slice(2, 3, None), slice(0, 1, None)), (slice(2, 3, None), slice(1, 2, None)),
+                   (slice(2, 3, None), slice(2, 3, None)), (slice(3, 4, None), slice(0, 1, None)),
+                   (slice(3, 4, None), slice(1, 2, None)), (slice(3, 4, None), slice(2, 3, None)))
+        self.assertEqual(actual, desired)
 
-        def update_map_slices(map_slices, key, target):
-            for idx, slc in enumerate(slices):
-                map_slices[idx][key] = target.__getitem__(slc)
-            return map_slices
-
-        t = Dimension('t', 35)
-        l = Dimension('z', 11)
-        y = Dimension('y', 7)
-        x = Dimension('x', 5)
-        dimensions = [t, l, y, x]
-        variable = Variable(name='distribute_me', dimensions=dimensions)
-        n_elements = reduce(lambda i, j: i * j, variable.shape)
-        value = np.arange(n_elements).reshape(variable.shape)
-        variable.value = value
-        # print variable.value
-
-        n = 5
-        actual = create_slices([len(d) for d in variable.dimensions[2:]], n)
-
-        vscatter = []
-        for idx in range(n):
-            y_slice = actual[0][idx]
-            x_slice = actual[1][idx]
-            vscatter.append(variable.__getitem__({'y': y_slice, 'x': x_slice}))
-
-            # path_shp_grid = self.get_temporary_file_path('grid.shp')
+        size = 5
+        shape = (4, 3)
+        actual = create_slices(size, shape)
+        for a in actual:
+            print a
 
     def test_get_local_to_global_slices(self):
         slices_global = (slice(2, 4, None), slice(0, 2, None))
@@ -144,19 +109,10 @@ class Test(AbstractTestNewInterface):
 
 def get_mpi_grid_get_intersects(grid, subset):
     if MPI_RANK == 0:
-        size_min = 2
-        if MPI_SIZE == 1:
-            distribute = [1, 1]
-        elif MPI_SIZE <= (size_min + 1):
-            distribute = [MPI_SIZE, 1]
-        else:
-            distribute = int(np.ceil(MPI_SIZE / size_min))
-            distribute = [distribute, MPI_SIZE - distribute]
-        distribute = [4, 3]
-        print distribute
-        assert distribute[0] * distribute[1] == MPI_SIZE
+        if MPI_SIZE != 1:
+            assert MPI_SIZE <= reduce(lambda x, y: x * y, grid.shape)
 
-        slices_global = create_slices([len(d) for d in grid.dimensions], distribute)
+        slices_global = create_slices(MPI_SIZE, grid.shape)
         g_scatter = [grid[slc] for slc in slices_global]
     else:
         g_scatter = None
